7c730f4fc2c280d3ee4dd1caca0ec6cc3330650a
NJUWayneChen <mf21320020@smail.nju.edu.cn>
2022-01-22 12:24:24 +0800

    feat: ChargeServiceImpl.updateDeviceInfo

--- src/test/java/com/example/connector/service/charger/ChargerServiceImplTest.java
+++ src/test/java/com/example/connector/service/charger/ChargerServiceImplTest.java
@@ -2,11 +2,13 @@ package com.example.connector.service.charger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.example.connector.bo.charger.ChargerDeviceInfoBo;
 import com.example.connector.entity.charger.ChargerDeviceInfo;
 import com.example.connector.repo.charger.ChargerDeviceInfoRepository;
 import com.example.connector.vo.charger.ChargerDeviceInfoVo;
 import java.util.*;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+@Slf4j
 @SpringBootTest
 class ChargerServiceImplTest {
 
@@ -31,6 +34,8 @@ class ChargerServiceImplTest {
     @MockBean private ChargerDeviceInfoRepository repository;
 
     @Autowired private ChargerService service;
+
+    @Autowired private ChargerServiceBiz serviceBiz;
 
     @BeforeEach
     void setUp() {
@@ -68,5 +73,43 @@ class ChargerServiceImplTest {
     }
 
     @Test
-    void updateDeviceInfo() {}
+    void updateDeviceInfo1() {
+        infos.get(0).setModifyCnt(0);
+
+        long updateId = 2;
+        short zdFcode = 1, cdFcode = 1;
+        String cdPid = "updated";
+        short outCnt = 5, phaseCnt = 5;
+        float inUe = 3.0f, inIe = 3.0f, inPower = 6.0f;
+        float outUe = 2.5f, outIe = 2.5f, outPower = 5.0f;
+        int datapoint = 1;
+        ChargerDeviceInfoBo update =
+                new ChargerDeviceInfoBo(
+                        updateId, zdFcode, cdFcode, cdPid, outCnt, phaseCnt, inUe, inIe, inPower,
+                        outUe, outIe, outPower, datapoint);
+
+        Mockito.when(repository.findById(updateId)).thenReturn(Optional.of(infos.get(0)));
+        Mockito.when(repository.save(infos.get(0))).thenReturn(infos.get(0));
+
+        serviceBiz.updateDeviceInfo(update);
+
+        assertEquals(cdPid, infos.get(0).getCdPid());
+        assertEquals(1, infos.get(0).getModifyCnt());
+    }
+
+    @Test
+    void updateDeviceInfo2() {
+        long updateId = 1;
+
+        ChargerDeviceInfoBo failed = Mockito.mock(ChargerDeviceInfoBo.class);
+        Mockito.when(repository.findById(updateId)).thenReturn(Optional.empty());
+
+        serviceBiz.updateDeviceInfo(failed);
+
+        // 验证确实没有调用update方法，也就是在调用前就捕捉到null并返回
+        for (ChargerDeviceInfo info : infos) {
+            Mockito.verify(failed, Mockito.never()).updateEntity(info);
+        }
+        assertEquals(2, infos.get(0).getId());
+    }
 }
