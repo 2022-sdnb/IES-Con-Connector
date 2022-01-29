c7daa28a937e78ccadb9df2dc35a2b22d2da34e7
Bomou He <paakmau@outlook.com>
2022-01-29 17:07:08 +0800

    test: rename a test

--- src/test/java/com/example/connector/gateway/device/DeviceGatewayTest.java
+++ src/test/java/com/example/connector/gateway/device/DeviceGatewayTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeviceGatewayImplTest {
+class DeviceGatewayTest {
     DeviceGateway gateway = new DeviceGatewayImpl();
 
     @BeforeAll
