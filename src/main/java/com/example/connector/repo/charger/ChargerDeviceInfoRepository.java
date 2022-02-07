d798178e2746eaf7c7ffc57b3cb924160b04906b
NJUWayneChen <mf21320020@smail.nju.edu.cn>
2022-02-07 18:04:53 +0800

    feat: repo

--- src/main/java/com/example/connector/repo/charger/ChargerDeviceInfoRepository.java
+++ src/main/java/com/example/connector/repo/charger/ChargerDeviceInfoRepository.java
@@ -3,4 +3,5 @@ package com.example.connector.repo.charger;
 import com.example.connector.entity.charger.ChargerDeviceInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 直接继承自Jpa提供的自实现接口
 public interface ChargerDeviceInfoRepository extends JpaRepository<ChargerDeviceInfo, Long> {}
